b938987c808245548337cab26800d6d7e4ae9182
Tadster-dev <65549004+Tadster-dev@users.noreply.github.com>
2020-06-13 08:19:25 -0400

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tad_b\documents\django class materials\btre_project\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
